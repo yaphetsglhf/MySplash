d1b03e47ebf1a88ee91eaf89315a0a5699f7f530
yn <yaphetsglhf@gmail.com>
2018-05-26 12:13:52 +0800

    🎨 去掉不需要的代码

--- hello.py
+++ hello.py
@@ -2,8 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import requests
-import time
-import os
 
 BASE_URL = 'http://localhost:8050/'
 RENDER_TYPE = 'render.png?'
@@ -24,17 +22,7 @@ request_url = BASE_URL + '%surl=%s&render_all=%s&quality=%s&timeout=%s&wait=%s' 
 print(request_url)
 
 res = requests.get(request_url)
-img = '111111111111.png'
+img = 'demo.png'
 
 with open(img, 'wb') as f:
     f.write(res.content)
-
-# print(request_url)
-# t1 = time.time()
-# img = 'emoji.jpeg'
-# cmd = "curl '%s' >> %s" % (request_url, img)
-#
-# os.system(cmd)
-
-# t2 = time.time()
-# print(t2 - t1)
